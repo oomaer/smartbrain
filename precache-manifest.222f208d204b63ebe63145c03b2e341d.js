bdf736e34eb00a4f6dd5d8fa1786ced21c698943
M Umair <cyberumiar751@gmail.com>
2019-07-20 06:47:07 +0500

    Updates

---
+++ precache-manifest.222f208d204b63ebe63145c03b2e341d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd1056073c229e55bff35ec82b425ea",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "3bd45ee57ffb8c206e61",
+    "url": "/smartbrain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "619a95509de0a6a068d1",
+    "url": "/smartbrain/static/css/main.5b32c828.chunk.css"
+  },
+  {
+    "revision": "3bd45ee57ffb8c206e61",
+    "url": "/smartbrain/static/js/2.19146b7c.chunk.js"
+  },
+  {
+    "revision": "619a95509de0a6a068d1",
+    "url": "/smartbrain/static/js/main.25b0d07e.chunk.js"
+  },
+  {
+    "revision": "c811ced7da05cfde01c3",
+    "url": "/smartbrain/static/js/runtime~main.54e658c3.js"
+  },
+  {
+    "revision": "a6bcaa5843e66f91a8fb973de588dd34",
+    "url": "/smartbrain/static/media/ai.a6bcaa58.png"
+  }
+]);
